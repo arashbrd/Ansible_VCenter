07b160dba6df8e4be843d8044861d2c62ca13730
Arash Bordbar <125759470+arashbrd@users.noreply.github.com>
2025-04-18 22:44:14 +0330

    Update setup.ps1
    delete comments

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,8 @@
 ﻿
 
-# نصب آنتی ویروس
 $antivirusInstaller = "C:\software\antivirus\Padvish.exe"  
 if (Test-Path $antivirusInstaller) {
     Start-Process -FilePath $antivirusInstaller -ArgumentList "/silent" -Wait
 }
 
-# حذف خودش بعد از اجرا (اختیاری)
 Remove-Item -Path $MyInvocation.MyCommand.Path -Force
